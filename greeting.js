92d7713dada116e1827ed72e99f8235b8c71f628
edwardcoded77 <eolagunju25@email.com>
2026-08-04 20:20:09 -0400

    fix the greeting comparison closes #9

---
+++ greeting.js
@@ -0,0 +1,9 @@
+// console.log("Welcomes everyone as Buttons");
+
+let visitor = "Maya";
+
+if (visitor === "Buttons") {
+  console.log("Welcome home, Buttons!");
+} else {
+  console.log("Welcome, " + visitor + "!");
+}
